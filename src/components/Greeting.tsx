5aba223fcd73b6cb16eabfaae9c558099efd7213
Chitsanupong <first30952@gmail.com>
2022-03-13 13:38:07 +0700

    feat: add responsive design in greeting

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -3,35 +3,36 @@ import Cake from '../assets/images/cake.png';
 
 const Greeting: FunctionComponent = () => {
   return (
-    <div class="grid place-items-center">
-      <div class="flex my-20 h-[240px] w-[1200px]">
-        <section class="flex flex-col justify-center">
-          <h4 class="font-dancing text-primary">What's new</h4>
-          <p class="font-garamond font-medium text-xl mt-2">
-            Chocolate Cream donuts with caramel topping
+    <div class="grid gap-x-6 gap-y-20 grid-cols-8 my-20">
+      <section class="self-center col-span-2 lg:col-span-3">
+        <h4 class="font-dancing text-primary">What's new</h4>
+        <p class="font-garamond font-medium text-xl mt-2">
+          Chocolate Cream donuts with caramel topping
+        </p>
+        <p class="font-garamond font-medium text-xl mt-2">
+          Raspberry pie with vanilla ice cream
+        </p>
+      </section>
+      <section class="col-span-4 lg:col-span-5 flex justify-center items-center">
+        <span class="h-full border-[1px] border-primary opacity-70 mr-12"></span>
+        <div>
+          <h4 class="italic">Hello! My name is Carlos. Your new sweet chef!</h4>
+          <p class="my-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            reiciendis ad officiis doloremque quis veniam ab quo odio animi
+            eligendi? Facere rerum sapiente quod odio dolor et! Reiciendis,
+            laborum quos?
           </p>
-        </section>
-        <span class="h-full border-[1px] border-primary opacity-70 mx-16"></span>
-        <section class="flex">
-          <div class="flex flex-col justify-center mr-16">
-            <h3 class="italic">
-              Hello! My name is Chitsanupong. Your new sweet chef!
-            </h3>
-            <p class="my-4">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
-              reiciendis ad officiis doloremque quis veniam ab quo odio animi
-              eligendi? Facere rerum sapiente quod odio dolor et! Reiciendis,
-              laborum quos?
-            </p>
-            <a href="#">view more</a>
-          </div>
-          <img
-            class="h-full w-[240px] object-cover"
-            src={Cake}
-            alt="sweet cake"
-          />
-        </section>
-      </div>
+          <a href="#" class="nav-text">
+            view more
+          </a>
+        </div>
+      </section>
+      <img
+        class="h-full lg:h-4/6 object-cover col-span-2 lg:col-span-8 lg:justify-self-center"
+        src={Cake}
+        alt="sweet cake"
+      />
     </div>
   );
 };
