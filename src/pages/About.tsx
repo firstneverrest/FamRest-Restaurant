e1e3b688eb8b5e78b2a2731cfff337b9e541ac12
Chitsanupong <first30952@gmail.com>
2022-03-22 20:27:27 +0700

    feat: add sample about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,32 @@
 import { h, FunctionComponent } from 'preact';
-import { Carousel } from '../components';
+import { Link } from 'preact-router/match';
+
+import { Footer } from '../components';
 
 const About: FunctionComponent = () => {
-  return <div class="">{/* <Carousel /> */}</div>;
+  return (
+    <div class="relative min-h-[100vh] pt-4">
+      <div className="mx-14">
+        <Link href="/">
+          <header class="mb-8">
+            <h3 class="font-semibold italic">FamRest</h3>
+            <p class="nav-text mb-4">Family Cooking Restaurant</p>
+          </header>
+        </Link>
+
+        <div class="flex flex-col">
+          <h1>About Page</h1>
+          <p class="mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            assumenda sapiente, nostrum dolores animi quam, nemo ad odio sit
+            maiores mollitia rem reprehenderit soluta quae sed blanditiis culpa
+            accusantium. Molestiae.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default About;
